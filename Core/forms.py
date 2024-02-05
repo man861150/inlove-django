f5c8a6d6c947ca963792ca8daea3854af8604447
man861150 <man861150@gmail.com>
2024-02-05 20:11:23 +0700

    remove label and set love days default

--- Core/forms.py
+++ Core/forms.py
@@ -3,4 +3,4 @@
 from django import forms
 
 class LoveCounterForm(forms.Form):
-    first_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label="Chọn ngày hẹn hò đầu tiên")
+    first_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label="")
--- Core/views.py
+++ Core/views.py
@@ -5,6 +5,11 @@ from datetime import datetime
 
 
 # Create your views here.
+def calculate_love_days(first_date):
+    today = datetime.now().date()
+    love_days = (today - first_date).days
+    return love_days
+
 def index(request):
     # people = Person.objects.all()
     # context = {'people': people}
@@ -14,14 +19,11 @@ def index(request):
         if form.is_valid():
 
             first_date = form.cleaned_data['first_date']
+            print(first_date)
             love_days = calculate_love_days(first_date)
             return render(request, 'index.html', {'love_counter_form': form, 'love_days': love_days})
     else:
-        form = LoveCounterForm()
-    return render(request, 'index.html', {'love_counter_form': form})
-
-def calculate_love_days(first_date):
-    today = datetime.now().date()
-    love_days = (today - first_date).days
-    return love_days
-
+        my_first_date = datetime.strptime("2024-01-30", "%Y-%m-%d").date()
+        love_days = calculate_love_days(my_first_date)
+        form = LoveCounterForm(initial={'first_date': my_first_date})
+    return render(request, 'index.html', {'love_counter_form': form, 'love_days': love_days})
